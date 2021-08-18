9abf699efd1e8ea07a90402b515b579482b8a967
samueldusek <smldsk@protonmail.com>
2021-08-18 19:45:49 +0200

    add basic styles to BookCard component

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 // Styles
-import styles from "../styles/Home.module.css";
+import styles from "../styles/BookCard.module.css";
 
 // Types
 import { Book } from "../libs/book";
@@ -13,17 +13,24 @@ type BookCardProps = {
 
 function BookCard({ book }: BookCardProps) {
   return (
-    <div>
-      <Image
-        src="/images/books/books1.png"
-        alt="Book"
-        width={200}
-        height={300}
-      />
-      <h1>{book.title}</h1>
-      <h5>Book author</h5>
-      <h5>{book.datePublished}</h5>
-      <h5>{book.isbn}</h5>
+    <div className={styles.BookCard}>
+      <div className={styles.box}>
+        <img
+          className={styles.image}
+          src="/images/books/books1.png"
+          alt="Book cover"
+        />
+        <div className={styles.text}>
+          <h3 className={styles.heading}>{book.title}</h3>
+          <h5 className={styles.author}>Samuel Dusek</h5>
+          <div className={styles.footer}>
+            <div>
+              <p className={styles.date}>{book.datePublished}</p>
+              <p className={styles.isbn}>{book.isbn}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
